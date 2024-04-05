3979e2d1e022da41aa3ee7ab14938d75b7dba81c
Yronusa <yronusa2000@gmail.com>
2024-04-05 15:11:59 +0200

    maj1

--- src/Graph.java
+++ src/Graph.java
@@ -16,21 +16,47 @@ public class Graph {
      * @return
      */
 
-    public Path shortestPath(Vertex debut, Vertex end){
+    public Path shortestPath(Vertex debut, Vertex end) {
 
-        List<Vertex> visitedVertices = new ArrayList<>();
+        List<Vertex> unvisitedVertices = this.vertices;
 
-        HashMap<Vertex, Integer> distanceToStartMap = new HashMap<>();
+        HashMap<Vertex, Long> distanceToStartMap = new HashMap<>();
         //Set the distance between the debut Vertex and all other Vertexs
         this.getVertexs().forEach(Vertex -> {
-            distanceToStartMap.put(Vertex,Integer.MAX_VALUE);
+            distanceToStartMap.put(Vertex, Long.MAX_VALUE);
         });
 
         // Set the distance between origin vertex and itself at 0
-        distanceToStartMap.put(debut, 0);
+        distanceToStartMap.put(debut, 0L);
 
 
+        while (!unvisitedVertices.isEmpty()) {
 
+            Vertex currentVertex = unvisitedVertices.stream().min(new Comparator<Vertex>() {
+                @Override
+                public int compare(Vertex o1, Vertex o2) {
+                    return 0;
+                }
+            })
+
+            // Getting all the neighbours of the vertex, that haven't yet been visited.
+            List<Vertex> unvisitedNeighbours = currentVertex
+                    .getAccessibleNeighbours()
+                    .stream()
+                    .filter(unvisitedVertices::contains)
+                    .toList();
+
+            // For each of those unvisited neighbours, update the distance if necessary.
+            unvisitedNeighbours.forEach(vertex -> {
+
+                int distanceToOrigin = distanceToStartMap.get(vertex) + distanceToStartMap.get(currentVertex);
+                if (distanceToStartMap.get(vertex) > distanceToOrigin) distanceToStartMap.put(vertex, distanceToOrigin);
+            });
+
+            visitedVertices.add(currentVertex);
+
+
+        }
 
         return null;
     }
